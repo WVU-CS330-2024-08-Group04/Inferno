b7b799cb14adafa2fbdbe5e457a80287543bc670
Tanner <tannerf0526@gmail.com>
2024-10-18 11:13:43 -0400

    Add Account Creation php code for SQL database

--- create_account.php
+++ create_account.php
@@ -1,48 +1,25 @@
-<!DOCTYPE html>  <!-- create_account.php -->
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Account Creation Page</title>
-</head>
-<body>
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
+        $conn = mysqli_connect('localhost','root','','accounts') or die("Connection Failed:" .mysqli_connect_error());
+        if(isset($_POST['username']) && isset($_POST['password'])){
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            // $Upper = "/[A-Z]/";
 
-    <?php
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            // if(strlen($password) < 8 && preg_match($Upper, $password)){
+                
+            // }
 
-            $username = $_POST["username"];
-            $password = $_POST["password"];
-        
-            $passwordHash = password_hash($password, PASSWORD_BCRYPT);
-        
-            $mysqli = new mysqli("localhost", "username", "password", "database");
-            $sql = "INSERT INTO Users VALUES ('" . $mysqli->real_escape_string($username) . "', '$passwordHash')";
-            if($mysqli->query($sql)){
-                echo "<p>Account Created.</p>",
-                    "<p><a href='login.php'>Login</a></p></html>";
-                die;
-            }
-            elseif($mysqli->errno == 1062){
-                echo "<p>The username already exists.","Please re-enter.</p>";
+            $sql = "INSERT INTO `accounts` (`username`, `password`) VALUES ('$username', '$password')";
+
+            $query = mysqli_query($conn, $sql);
+            if($query){
+                echo 'Entry Successful';
             }
             else{
-                die("Error");
+                echo 'Error Occurred';
             }
         }
-        
-    ?>
-
-    <form method = "post" action="create_account.php">
-        <p>
-            <label for="username">Username:</label>
-            <input type="text" name="username" id="username" autofocus>
-        </p>
-        <p>
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password">
-        </p>
-        <input type="submit" value="Create Account">
-    </form>
-</body>
-</html>
+    }
+?>    
 
