033f10d9c49c28acf8aa41e804a1891906ceb8b8
sms00078 <157189932+sms00078@users.noreply.github.com>
2024-11-01 13:56:49 -0400

    Update dark_mode.js

--- dark_mode.js
+++ dark_mode.js
@@ -8,10 +8,12 @@ function darkMode() {
     if (element.classList.contains("dark-mode")) {
         clickMeText.textContent = "Light Mode";
         buttonIcon.setAttribute("name", "sunny");
+        document.getElementById('image').src ='Dark Mode Inferno Full Logo cropped.png';
         localStorage.setItem('theme', 'dark'); // Save preference
     } else {
         clickMeText.textContent = "Dark Mode";
         buttonIcon.setAttribute("name", "moon");
+        document.getElementById('image').src ='Inferno wildfire full logo cropped.png';
         localStorage.setItem('theme', 'light'); // Save preference
     }
 
@@ -25,5 +27,5 @@ function darkMode() {
         }
     }
     
-    window.onload = setInitialTheme;
+    window.onload(setInitialTheme());
 
